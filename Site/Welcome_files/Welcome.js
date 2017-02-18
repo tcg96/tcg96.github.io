1e0c688cbb3e7d63ab87572e6b600f3d0eefbe2a
tcg96 <ale12396@gmail.com>
2017-02-19 00:08:29 +0100

    got rid of the crappy iWeb parts

--- Site/Welcome_files/Welcome.js
+++
@@ -1,9 +0,0 @@
-// Created by iWeb 3.0.2 local-build-20161014
-
-setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
-{return false;}
-function onPageLoad()
-{loadMozillaCSS('Welcome_files/WelcomeMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');detectBrowser();Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');fixupIECSS3Opacity('id3');performPostEffectsFixups()}
-function onPageUnload()
-{Widget.onunload();}
